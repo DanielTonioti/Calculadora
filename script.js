faad98866f0154dfd860bac7341d50f8eed8a87a
DanielTonioti <daniel_tonioti@estudante.sesisenai.org.br>
2026-03-25 19:48:20 -0300

    UP - Calculadora funcionando, faltando desafios

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 function Values(){
-    let Valor01 = document.getElementById('numero01').value;
-    let Valor02 = document.getElementById('numero02').value;
+    let Valor01 = parseFloat(document.getElementById('Numero01').value);
+    let Valor02 = parseFloat(document.getElementById('Numero02').value);
 
-    if(Valor01 == "" || Valor02 == ""){
+    if(!Valor01 || !Valor02){
         alert("Preencha os campos vazios");
+        return null;
     }
     if (isNaN(Valor01) || isNaN(Valor02)){
         alert("Insira um valor válido");
         return null;
     }
+    return { Valor01, Valor02 };
 }
 function add(){
     let valores=Values();
+    if(valores == null){
+        return;
+    }
     let soma = valores.Valor01 + valores.Valor02;
     alert("O resultado é: " + soma);
 }
 function sub(){
     let valores=Values();
+    if(valores == null){
+        return;
+    }
     let subtraction= valores.Valor01-valores.Valor02;
     alert("O resultado é: " + subtraction);
 }
 function mult(){
     let valores=Values();
+    if(valores == null){
+        return;
+    }
     let multiplication=valores.Valor01*valores.Valor02;
     alert("O resultado é: " + multiplication);
 }
 function div(){
     let valores = Values();
-    if(valores.Valor01 == 0 || valores.Valor02 == 0){
+    if(valores == null){
+        return;
+    }
+    if(valores.Valor02 == 0){
         alert("0 Não é permitido para divisões");
         return;
     }else{
